9fa288efc8966275ca53eb3122a46af6a980db3c
Hoyoun (Harry) Lee <h344lee@uwaterloo.ca>
2022-02-01 17:03:32 -0500

    Initial commit

---
+++ 01-Adapter/D_CLDASST_Bu_Mapper.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+
+current_path = os.getcwd()
+bu_df = pd.read_csv(current_path + '\\ad_dump\\AD_Extract.csv')
+
+bu_df.drop('User Name', axis=1, inplace=True)
+number_list = ['USR_' + str(num) for num in range(1, len(bu_df)+1)]
+
+bu_df.insert(0, "USR_ID", number_list)
+bu_df.rename(columns={'Display Name': 'USR_NM'}, inplace=True)
+bu_df.rename(columns={'Department': 'USR_BUS_UNT'}, inplace=True)
+bu_df['USR_STAT'] = ""
+bu_df['USR_LOC'] = ""
+bu_df['USR_SUB'] = ""
+bu_df['USR_CAT'] = ""
+bu_df['USR_SAS_SKL'] = ""
+bu_df['USR_SAS_AGE'] = ""
+bu_df['USR_NON_SAS_SKL'] = ""
+bu_df['USR_NON_SAS_AGE'] = ""
+
+bu_df.to_csv('..\\00-Data Model\\D_CLDASST_Bu_Mapper_Output.csv', index=False)
