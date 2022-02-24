a5ff661252f8e995d0afa9d085b9201eeb028e89
Harry Lee <h344lee@uwaterloo.ca>
2022-02-24 14:20:30 -0500

    linux enabled

---
+++ Adapter/D_CLDASST_Bu_Mapper.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import platform
+
+current_path = os.getcwd()
+if platform.system() == 'Windows':
+    bu_df = pd.read_csv(current_path + '\\ad_dump\\AD_Extract.csv')
+else:
+    bu_df = pd.read_csv(current_path + '/ad_dump/AD_Extract.csv')
+bu_df.drop('User Name', axis=1, inplace=True)
+number_list = ['USR_' + str(num) for num in range(1, len(bu_df)+1)]
+
+bu_df.insert(0, "USR_ID", number_list)
+bu_df.rename(columns={'Display Name': 'USR_NM'}, inplace=True)
+bu_df.rename(columns={'Department': 'USR_BUS_UNT'}, inplace=True)
+bu_df['USR_STAT'] = ""
+bu_df['USR_LOC'] = ""
+bu_df['USR_SUB'] = ""
+bu_df['USR_CAT'] = ""
+bu_df['USR_SAS_SKL'] = ""
+bu_df['USR_SAS_AGE'] = ""
+bu_df['USR_NON_SAS_SKL'] = ""
+bu_df['USR_NON_SAS_AGE'] = ""
+
+if platform.system() == 'Windows':
+    if not os.path.isdir("..\\Data_Model\\Extracted_Files"):
+        os.makedirs("..\\Data_Model\\Extracted_Files")
+    bu_df.to_csv('..\\Data_Model\\Extracted_Files\\D_CLDASST_DISC_USR_GRP_REF.csv', index=False)
+else :
+    if not os.path.isdir("../Data_Model/Extracted_Files"):
+        os.makedirs("../Data_Model/Extracted_Files")
+    bu_df.to_csv('../Data_Model/Extracted_Files/D_CLDASST_DISC_USR_GRP_REF.csv', index=False)
